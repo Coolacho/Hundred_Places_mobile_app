6f5c78159ca3d12d4b0d6bd1482f1d4d52ab4c1b
Angel Angelov <sofiaangel35@gmail.com>
2024-04-07 01:01:38 +0300

    Created application class to use new app container and added it to the manifest

---
+++ app/src/main/java/com/example/hundredplaces/HundredPlacesApplication.kt
@@ -0,0 +1,16 @@
+package com.example.hundredplaces
+
+import android.app.Application
+import com.example.hundredplaces.data.containers.DefaultAppContainer
+
+class HundredPlacesApplication : Application() {
+    /**
+     * AppContainer instance used by the rest of classes to obtain dependencies
+     */
+    private lateinit var container: DefaultAppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer(this)
+    }
+}
