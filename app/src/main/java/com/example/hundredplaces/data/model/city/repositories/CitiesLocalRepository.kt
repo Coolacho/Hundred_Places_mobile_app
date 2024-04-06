241f89206914c7c5a764998da6beedebe6e14b9e
Angel Angelov <sofiaangel35@gmail.com>
2024-04-06 12:29:21 +0300

    Removed unnecessary javadoc

--- app/src/main/java/com/example/hundredplaces/data/model/city/repositories/CitiesLocalRepository.kt
+++ app/src/main/java/com/example/hundredplaces/data/model/city/repositories/CitiesLocalRepository.kt
@@ -4,32 +4,15 @@ import com.example.hundredplaces.data.model.city.City
 import com.example.hundredplaces.data.model.city.CityDao
 import kotlinx.coroutines.flow.Flow
 
-/**
- * Repository that provides insert, update, delete, and retrieve of [City] from a local data source.
- */
 class CitiesLocalRepository(private val cityDao: CityDao) : CitiesRepository {
-    /**
-     * Retrieve all the cities from the local data source.
-     */
+
     override suspend fun getAllCitiesStream(): Flow<List<City>> = cityDao.getAllCities()
 
-    /**
-     * Retrieve an city from the local data source that matches with the [id].
-     */
     override suspend fun getCityStream(id: Int): Flow<City?> = cityDao.getCity(id)
 
-    /**
-     * Insert city in the local data source
-     */
     override suspend fun insertCity(city: City) = cityDao.insert(city)
 
-    /**
-     * Delete city from the local data source
-     */
     override suspend fun deleteCity(city: City) = cityDao.delete(city)
 
-    /**
-     * Update city in the local data source
-     */
     override suspend fun updateCity(city: City) = cityDao.update(city)
 }
--- app/src/main/java/com/example/hundredplaces/data/model/city/repositories/CitiesRemoteRepository.kt
+++ app/src/main/java/com/example/hundredplaces/data/model/city/repositories/CitiesRemoteRepository.kt
@@ -4,34 +4,17 @@ import com.example.hundredplaces.data.model.city.City
 import com.example.hundredplaces.data.model.city.CityRestApiService
 import kotlinx.coroutines.flow.Flow
 
-/**
- * Repository that provides insert, update, delete, and retrieve of [City] from a remote data source.
- */
 class CitiesRemoteRepository(
     private val cityRestApiService: CityRestApiService
 ) : CitiesRepository {
-    /**
-     * Retrieve all the cities from the remote data source.
-     */
+
     override suspend fun getAllCitiesStream(): Flow<List<City>> = cityRestApiService.getAllCities()
 
-    /**
-     * Retrieve an city from the remote data source that matches with the [id].
-     */
     override suspend fun getCityStream(id: Int): Flow<City?> = cityRestApiService.getCity(id)
 
-    /**
-     * Insert city in the remote data source
-     */
     override suspend fun insertCity(city: City) = cityRestApiService.insertCity(city)
 
-    /**
-     * Delete city from the remote data source
-     */
     override suspend fun deleteCity(city: City) = cityRestApiService.deleteCity(city)
 
-    /**
-     * Update city in the remote data source
-     */
     override suspend fun updateCity(city: City) = cityRestApiService.updateCity(city)
 }
