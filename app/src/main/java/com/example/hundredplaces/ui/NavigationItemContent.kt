0016566b98aec89872873c10c1cc4866dcc82873
Angel Angelov <sofiaangel35@gmail.com>
2024-04-08 01:32:05 +0300

    Replaced NavigationItemContent.kt with NavigationDestination

--- app/src/main/java/com/example/hundredplaces/ui/NavigationItemContent.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hundredplaces.ui
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class NavigationItemContent(
-    val icon: ImageVector,
-    val text: String
-)
