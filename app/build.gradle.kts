3b4c325b282077a6dd3affa8c596df5a271eb4c0
Angel Angelov <sofiaangel35@gmail.com>
2024-04-07 19:27:35 +0300

    Added window size class dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //WindowSizeClass
+    implementation(libs.androidx.material3.window.size)
     //Datastore
     implementation(libs.androidx.datastore.preferences)
     //Room
