63cddff2358f2b264f4bc187d3d5ca878ceb70b0
Angel Angelov <sofiaangel35@gmail.com>
2024-04-08 01:29:02 +0300

    Added viewmodel and state for layout change

---
+++ app/src/main/java/com/example/hundredplaces/ui/home/HomeUiState.kt
@@ -0,0 +1,11 @@
+package com.example.hundredplaces.ui.home
+
+import com.example.hundredplaces.R
+
+data class HomeUiState(
+    val isLinearLayout: Boolean = true,
+    val toggleContentDescription: Int =
+        if (isLinearLayout) R.string.grid_layout_toggle else R.string.linear_layout_toggle,
+    val toggleIcon: Int =
+        if (isLinearLayout) R.drawable.ic_grid_layout else R.drawable.ic_linear_layout
+)
---
+++ app/src/main/java/com/example/hundredplaces/ui/home/HomeViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.hundredplaces.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hundredplaces.data.UserPreferencesRepository
+import com.example.hundredplaces.ui.navigation.NavigationDestination
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(HomeUiState())
+
+    // UI states access for various [HomeUiState]
+    val uiState: StateFlow<HomeUiState> =
+        userPreferencesRepository.isLinearLayout
+            .map { isLinearLayout ->
+                HomeUiState(isLinearLayout = isLinearLayout)
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = HomeUiState()
+            )
+
+    /*
+     * [selectLayout] change the layout and icons accordingly and
+     * save the selection in DataStore through [userPreferencesRepository]
+     */
+    fun selectLayout(isLinearLayout: Boolean) {
+        viewModelScope.launch {
+            userPreferencesRepository.saveLayoutPreference(isLinearLayout)
+        }
+    }
+
+}
