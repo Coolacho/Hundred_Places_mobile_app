bbc59b8de34d4c1557160a22cf26967ce4b127cb
Angel Angelov <sofiaangel35@gmail.com>
2024-04-07 15:25:48 +0300

    Removed unused import

--- app/src/androidTest/java/com/example/hundredplaces/PlaceDaoTest.kt
+++ app/src/androidTest/java/com/example/hundredplaces/PlaceDaoTest.kt
@@ -1,7 +1,6 @@
 package com.example.hundredplaces
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
