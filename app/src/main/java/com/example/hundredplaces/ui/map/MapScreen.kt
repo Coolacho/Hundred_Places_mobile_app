696764179cfd1693c11387fa96cbab107ab6f2c6
Angel Angelov <sofiaangel35@gmail.com>
2024-04-09 00:50:52 +0300

    Started to add map view

--- app/src/main/java/com/example/hundredplaces/ui/map/MapScreen.kt
+++ app/src/main/java/com/example/hundredplaces/ui/map/MapScreen.kt
@@ -1,11 +1,15 @@
 package com.example.hundredplaces.ui.map
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.hundredplaces.R
 import com.example.hundredplaces.ui.navigation.NavigationDestination
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 object MapDestination : NavigationDestination {
     override val route = "Map"
@@ -19,7 +23,20 @@ object MapDestination : NavigationDestination {
 fun MapScreen(
     modifier: Modifier = Modifier
 ) {
-    Row {
-        Text(text = "This is the map screen")
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context ->
+            MapView(context).apply {
+                id = R.id.map_view
+            }
+        }
+    ) { mapView ->
+        mapView.getMapAsync { googleMap ->
+            val coordinates = LatLng(37.7749, -122.4194) // San Francisco
+            googleMap.addMarker(
+                MarkerOptions().position(coordinates).title("Marker in San Francisco")
+            )
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates))
+        }
     }
 }
