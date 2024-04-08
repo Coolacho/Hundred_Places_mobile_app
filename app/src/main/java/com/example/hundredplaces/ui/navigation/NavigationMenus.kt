8a7d66ef0b38104a77b26f8fc90c7e440f6a54c8
Angel Angelov <sofiaangel35@gmail.com>
2024-04-08 14:04:28 +0300

    Integrated navController with navigation menus

--- app/src/main/java/com/example/hundredplaces/ui/navigation/NavigationMenus.kt
+++ app/src/main/java/com/example/hundredplaces/ui/navigation/NavigationMenus.kt
@@ -1,6 +1,9 @@
 package com.example.hundredplaces.ui.navigation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -11,24 +14,44 @@ import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.hundredplaces.R
 
 @Composable
 fun AppBottomNavigationBar(
-    currentDestination: String,
     navigationItemContentList: List<NavigationDestination>,
-    onTabPressed: (NavigationDestination) -> Unit,
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
     NavigationBar(modifier = modifier) {
         for (navItem in navigationItemContentList) {
             NavigationBarItem(
-                selected = currentDestination == navItem.route,
-                onClick = { },
+                selected = currentDestination?.hierarchy?.any { it.route == navItem.route } == true,
+                onClick = { navController.navigate(navItem.route){
+                    // Pop up to the start destination of the graph to
+                    // avoid building up a large stack of destinations
+                    // on the back stack as users select items
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    // Avoid multiple copies of the same destination when
+                    // re-selecting the same item
+                    launchSingleTop = true
+                    // Restore state when re-selecting a previously selected item
+                    restoreState = true
+                }},
                 icon = {
                     Icon(
                         painter = painterResource(navItem.iconRes),
@@ -42,16 +65,29 @@ fun AppBottomNavigationBar(
 
 @Composable
 fun AppNavigationRail(
-    currentDestination: String,
     navigationItemContentList: List<NavigationDestination>,
-    onTabPressed: (NavigationDestination) -> Unit,
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
     NavigationRail(modifier = modifier) {
         for (navItem in navigationItemContentList) {
             NavigationRailItem(
-                selected = currentDestination == navItem.route,
-                onClick = {  },
+                selected = currentDestination?.hierarchy?.any { it.route == navItem.route } == true,
+                onClick = { navController.navigate(navItem.route){
+                    // Pop up to the start destination of the graph to
+                    // avoid building up a large stack of destinations
+                    // on the back stack as users select items
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    // Avoid multiple copies of the same destination when
+                    // re-selecting the same item
+                    launchSingleTop = true
+                    // Restore state when re-selecting a previously selected item
+                    restoreState = true
+                }},
                 icon = {
                     Icon(
                         painter = painterResource(navItem.iconRes),
@@ -65,15 +101,38 @@ fun AppNavigationRail(
 
 @Composable
 fun NavigationDrawerContent(
-    currentDestination: String,
     navigationItemContentList: List<NavigationDestination>,
-    onTabPressed: (NavigationDestination) -> Unit,
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
     Column(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = stringResource(id = R.string.app_name))
+        }
         for (navItem in navigationItemContentList) {
             NavigationDrawerItem(
-                selected = currentDestination == navItem.route,
+                selected = currentDestination?.hierarchy?.any { it.route == navItem.route } == true,
+                onClick = { navController.navigate(navItem.route){
+                    // Pop up to the start destination of the graph to
+                    // avoid building up a large stack of destinations
+                    // on the back stack as users select items
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    // Avoid multiple copies of the same destination when
+                    // re-selecting the same item
+                    launchSingleTop = true
+                    // Restore state when re-selecting a previously selected item
+                    restoreState = true
+                }},
                 label = {
                     Text(
                         text = navItem.route,
@@ -88,8 +147,7 @@ fun NavigationDrawerContent(
                 },
                 colors = NavigationDrawerItemDefaults.colors(
                     unselectedContainerColor = Color.Transparent
-                ),
-                onClick = {  }
+                )
             )
         }
     }
