a8285334091ce9ad699935b1b511ec27406e8ef6
Angel Angelov <sofiaangel35@gmail.com>
2024-04-12 01:08:38 +0300

    Added logic for creating multiple map markers based on data source

--- app/src/main/java/com/example/hundredplaces/ui/map/MapScreen.kt
+++ app/src/main/java/com/example/hundredplaces/ui/map/MapScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
@@ -18,12 +20,14 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hundredplaces.R
+import com.example.hundredplaces.data.FakePlaceDataSource
+import com.example.hundredplaces.data.model.place.PlaceWithCityAndImages
 import com.example.hundredplaces.ui.navigation.NavigationDestination
 import com.example.hundredplaces.ui.theme.HundredPlacesTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MarkerInfoWindowContent
+import com.google.maps.android.compose.MarkerInfoWindow
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
@@ -37,7 +41,9 @@ object MapDestination : NavigationDestination {
  */
 @Composable
 fun MapScreen(
-    modifier: Modifier = Modifier
+    navigateToPlaceEntry: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    placesWithCityAndImages: List<PlaceWithCityAndImages> = FakePlaceDataSource.PlacesList
 ) {
     val sofia = LatLng(42.65542986025372, 23.271044935725428)
     val cameraPositionState = rememberCameraPositionState{
@@ -48,20 +54,26 @@ fun MapScreen(
             .fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
-        MarkerInfoWindowContent(
-            state = MarkerState(position = sofia)
-        ){
-            PlaceMapMarker()
+        for (place in placesWithCityAndImages) {
+            MarkerInfoWindow(
+                state = MarkerState(position = LatLng(place.place.latitude, place.place.longitude)),
+                onInfoWindowClick = { navigateToPlaceEntry(place.place.id) }
+            ){
+                PlaceMapMarker(place)
+            }
         }
     }
 }
 
 @Composable
 fun PlaceMapMarker(
+    placeWithCityAndImages: PlaceWithCityAndImages,
     modifier: Modifier = Modifier
 ) {
     OutlinedCard(
         modifier = modifier
+            .width(300.dp)
+            .height(200.dp)
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -72,6 +84,7 @@ fun PlaceMapMarker(
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxWidth()
+                    .weight(1f)
             )
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -79,12 +92,12 @@ fun PlaceMapMarker(
                     .padding(vertical = 8.dp)
             ) {
                 Text(
-                    text = "13. Национален исторически музей",
+                    text = placeWithCityAndImages.place.name,
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.titleMedium
                 )
                 Text(
-                    text = "гр. София",
+                    text = placeWithCityAndImages.city,
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.bodyMedium
                 )
@@ -101,7 +114,7 @@ fun PlaceMapMarker(
 private fun PlaceMapMarkerPreview() {
     HundredPlacesTheme {
         Surface {
-            PlaceMapMarker()
+            PlaceMapMarker(FakePlaceDataSource.PlacesList[0])
         }
     }
 }
