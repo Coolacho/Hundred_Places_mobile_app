a7d7faa05e5a837f67d454726322654710e538f2
Angel Angelov <sofiaangel35@gmail.com>
2024-04-12 00:08:29 +0300

    Changed map logic to use compose libs

--- app/src/main/java/com/example/hundredplaces/ui/map/MapScreen.kt
+++ app/src/main/java/com/example/hundredplaces/ui/map/MapScreen.kt
@@ -1,15 +1,31 @@
 package com.example.hundredplaces.ui.map
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hundredplaces.R
 import com.example.hundredplaces.ui.navigation.NavigationDestination
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.MapView
+import com.example.hundredplaces.ui.theme.HundredPlacesTheme
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MarkerInfoWindowContent
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 object MapDestination : NavigationDestination {
     override val route = "Map"
@@ -23,20 +39,69 @@ object MapDestination : NavigationDestination {
 fun MapScreen(
     modifier: Modifier = Modifier
 ) {
-    AndroidView(
-        modifier = modifier.fillMaxSize(),
-        factory = { context ->
-            MapView(context).apply {
-                id = R.id.map_view
+    val sofia = LatLng(42.65542986025372, 23.271044935725428)
+    val cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(sofia, 10f)
+    }
+    GoogleMap(
+        modifier = modifier
+            .fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        MarkerInfoWindowContent(
+            state = MarkerState(position = sofia)
+        ){
+            PlaceMapMarker()
+        }
+    }
+}
+
+@Composable
+fun PlaceMapMarker(
+    modifier: Modifier = Modifier
+) {
+    OutlinedCard(
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.nim),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(
+                    text = "13. Национален исторически музей",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = "гр. София",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
         }
-    ) { mapView ->
-        mapView.getMapAsync { googleMap ->
-            val coordinates = LatLng(37.7749, -122.4194) // San Francisco
-            googleMap.addMarker(
-                MarkerOptions().position(coordinates).title("Marker in San Francisco")
-            )
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates))
+    }
+}
+
+/**
+ * Preview for place marker card
+ */
+@Preview
+@Composable
+private fun PlaceMapMarkerPreview() {
+    HundredPlacesTheme {
+        Surface {
+            PlaceMapMarker()
         }
     }
 }
