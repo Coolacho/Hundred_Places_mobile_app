5597b7ca2868c3108b293097efbd2119d77d8001
Angel Angelov <sofiaangel35@gmail.com>
2024-04-16 00:46:45 +0300

    Created broadcast receiver to respond to notification from geofences

---
+++ app/src/main/java/com/example/hundredplaces/ui/map/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,98 @@
+package com.example.hundredplaces.ui.map
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.hundredplaces.MainActivity
+import com.example.hundredplaces.R
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+
+const val TAG = "Geofence intent"
+
+class GeofenceBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        val geofencingEvent = intent?.let { GeofencingEvent.fromIntent(it) }
+        geofencingEvent?.let {
+            if (it.hasError()) {
+                val errorMessage = GeofenceStatusCodes
+                    .getStatusCodeString(it.errorCode)
+                Log.e(TAG, errorMessage)
+                return
+            }
+        }
+
+        // Get the transition type.
+        val geofenceTransition = geofencingEvent?.geofenceTransition
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            val triggeringGeofences = geofencingEvent.triggeringGeofences
+
+
+            // Send notification and log the transition details.
+            sendNotification(context)
+            Log.i(TAG, "One or more places nearby")
+        } else {
+            // Log the error.
+            Log.e(TAG, "Invalid geofence transition.")
+        }
+    }
+
+    private fun sendNotification(context: Context) {
+
+        // Make a channel if necessary
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        val name = "Nearby places"
+        val description = "Send notification when user is near a place"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel("GEOFENCE_NOTIFICATION", name, importance)
+        channel.description = description
+
+        // Add the channel
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+        notificationManager?.createNotificationChannel(channel)
+
+        //Create intent to navigate when tapping the notification
+        val intent = Intent(context, MainActivity::class.java)
+            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            .putExtra("navigateToPlacesDetails", true)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, "GEOFENCE_NOTIFICATION")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Places nearby")
+            .setContentText("One or more places nearby")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVibrate(LongArray(0))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        // Show the notification
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(context).notify(1, builder.build())
+        }
+
+    }
+}
