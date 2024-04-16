63326ae46db72743989b1ecea5ec858113cfb5bf
Angel Angelov <sofiaangel35@gmail.com>
2024-04-16 23:51:35 +0300

    Removed unused import

--- app/src/main/java/com/example/hundredplaces/data/model/user/UserWithVisits.kt
+++ app/src/main/java/com/example/hundredplaces/data/model/user/UserWithVisits.kt
@@ -3,7 +3,6 @@ package com.example.hundredplaces.data.model.user
 import androidx.room.Embedded
 import androidx.room.Relation
 import com.example.hundredplaces.data.model.visit.Visit
-import java.time.LocalDateTime
 
 data class UserWithVisits (
     @Embedded
