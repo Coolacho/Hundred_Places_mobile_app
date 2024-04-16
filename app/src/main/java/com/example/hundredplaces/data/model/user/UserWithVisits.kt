59636af4c9bc0306df5d6ad18ee6e021a8010f88
Angel Angelov <sofiaangel35@gmail.com>
2024-04-16 23:48:39 +0300

    Fixed relation fields

--- app/src/main/java/com/example/hundredplaces/data/model/user/UserWithVisits.kt
+++ app/src/main/java/com/example/hundredplaces/data/model/user/UserWithVisits.kt
@@ -2,16 +2,16 @@ package com.example.hundredplaces.data.model.user
 
 import androidx.room.Embedded
 import androidx.room.Relation
+import com.example.hundredplaces.data.model.visit.Visit
 import java.time.LocalDateTime
 
 data class UserWithVisits (
     @Embedded
     val user: User,
     @Relation(
-        parentColumn = "user_id",
-        entityColumn = "id",
-        projection = ["place_id, date_visited"],
-        entity = User::class
+        parentColumn = "id",
+        entityColumn = "user_id",
+        entity = Visit::class
     )
-    val visits: Map<Long, List<LocalDateTime>>
+    val visits: List<Visit>
 )
