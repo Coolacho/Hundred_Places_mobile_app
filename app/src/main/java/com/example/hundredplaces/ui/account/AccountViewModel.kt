2d7bda8ba8a478deec6510320bcbae9e63dae06c
Angel Angelov <sofiaangel35@gmail.com>
2024-04-22 21:12:24 +0300

    Added methods for creating and auto- and normal logging in an user

--- app/src/main/java/com/example/hundredplaces/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/example/hundredplaces/ui/account/AccountViewModel.kt
@@ -8,6 +8,7 @@ import com.example.hundredplaces.data.model.user.User
 import com.example.hundredplaces.data.model.user.repositories.UsersDataRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -19,8 +20,62 @@ class AccountViewModel(
     private val _uiState = MutableStateFlow(AccountUiState())
     val uiState: StateFlow<AccountUiState> = _uiState
 
+    init {
+        checkAutoLogin()
+    }
+
+    private fun checkAutoLogin() {
+        viewModelScope.launch{
+            if (userPreferencesRepository.prefUsername.first() != "") {
+                val user = userDataRepository.getUserByEmail(userPreferencesRepository.prefUsername.first())
+                _uiState.update {
+                    it.copy(
+                        isLoggedIn = true,
+                        userDetails = user,
+                        currentUser = user
+                    )
+                }
+            }
+        }
+    }
+
+    fun logIn(): Boolean {
+        if (validateInput()) {
+            viewModelScope.launch {
+                val user = userDataRepository.getUserByEmailAndPassword(uiState.value.userDetails.email, uiState.value.userDetails.password)
+                if (validateInput(user)) {
+                    _uiState.update {
+                        it.copy(
+                            userDetails = user,
+                            currentUser = user,
+                            isLoggedIn = true
+                        )
+                    }
+                    userPreferencesRepository.saveUsernamePreference(user.email)
+                }
+                else {
+                    _uiState.update {
+                        it.copy(
+                            userDetails = User(name = "", email = "", password = ""),
+                            isLoginSuccessful = false
+                        )
+                    }
+                }
+            }
+            return uiState.value.isLoggedIn
+        }
+        return false
+    }
+
     fun logOut() {
         viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    userDetails = User(name = "", email = "", password = ""),
+                    currentUser = User(name = "", email = "", password = ""),
+                    isLoggedIn = false
+                )
+            }
             userPreferencesRepository.saveUsernamePreference("")
         }
     }
@@ -33,18 +88,34 @@ class AccountViewModel(
         }
     }
 
+    fun createUser() {
+        if (validateInput()) {
+            viewModelScope.launch{
+                userDataRepository.insertUser(uiState.value.userDetails)
+                _uiState.update {
+                    it.copy(
+                        currentUser = uiState.value.userDetails,
+                        isLoggedIn = true
+                    )
+                }
+                userPreferencesRepository.saveUsernamePreference(uiState.value.userDetails.email)
+            }
+        }
+    }
+
     fun updateUiState(userDetails: User) {
         _uiState.update {
             it.copy(
                 userDetails = userDetails,
-                isEntryValid = validateInput(userDetails)
+                isEntryValid = validateInput(userDetails),
             )
         }
     }
 
-    private fun validateInput(userDetails: User = uiState.value.userDetails): Boolean {
+    fun validateInput(userDetails: User = uiState.value.userDetails): Boolean {
         return with(userDetails) {
-            name.isNotBlank() && email.isNotBlank() && password.isNotBlank()
+            if (uiState.value.isLoggedIn) name.isNotBlank() && email.isNotBlank() && password.isNotBlank()
+            else email.isNotBlank() && password.isNotBlank()
         }
     }
 }
