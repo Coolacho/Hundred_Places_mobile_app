051ec18f107557bc50b8f7392e6fd96f53d5d0e7
Angel Angelov <sofiaangel35@gmail.com>
2024-04-22 21:04:58 +0300

    Refactor getPlaces(); added methods for fetching visits

--- app/src/main/java/com/example/hundredplaces/ui/places/PlacesUiState.kt
+++ app/src/main/java/com/example/hundredplaces/ui/places/PlacesUiState.kt
@@ -1,8 +1,16 @@
 package com.example.hundredplaces.ui.places
 
+import com.example.hundredplaces.data.model.place.Place
+import com.example.hundredplaces.data.model.place.PlaceTypeEnum
 import com.example.hundredplaces.data.model.place.PlaceWithCityAndImages
+import com.google.android.gms.location.Geofence
 
 data class PlacesUiState(
     val places: List<PlaceWithCityAndImages> = listOf(),
-    val currentSelectedPlace: PlaceWithCityAndImages? = null,
+    val currentSelectedPlace: PlaceWithCityAndImages = PlaceWithCityAndImages(
+        place = Place(name = "", latitude = 0.0, longitude = 0.0, rating = 0.0, descriptionPath = "", is100Places = false, type = PlaceTypeEnum.OTHER, cityId = 0),
+        city = "",
+        images = listOf()
+    ),
+    val triggeredGeofences: List<Geofence> = listOf()
 )
--- app/src/main/java/com/example/hundredplaces/ui/places/PlacesViewModel.kt
+++ app/src/main/java/com/example/hundredplaces/ui/places/PlacesViewModel.kt
@@ -4,20 +4,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.hundredplaces.data.model.place.PlaceWithCityAndImages
 import com.example.hundredplaces.data.model.place.repositories.PlacesDataRepository
+import com.example.hundredplaces.data.model.user.User
+import com.example.hundredplaces.data.model.visit.Visit
+import com.example.hundredplaces.data.model.visit.repositories.VisitsDataRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 class PlacesViewModel (
-    private val placesDataRepository: PlacesDataRepository
+    private val placesDataRepository: PlacesDataRepository,
+    private val visitsDataRepository: VisitsDataRepository
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(PlacesUiState())
     val uiState: StateFlow<PlacesUiState> = _uiState
 
     init {
-        //getPlaces()
+        getPlaces()
     }
 
     fun selectPlaceCard(placeWithCityAndImages: PlaceWithCityAndImages) {
@@ -30,14 +35,23 @@ class PlacesViewModel (
 
     private fun getPlaces() {
         viewModelScope.launch {
-            placesDataRepository.getAllPlacesWithCityAndImagesStream().collect { places ->
-                _uiState.update {
-                    it.copy(
-                        places = places
-                    )
-                }
+            _uiState.update {
+                it.copy(
+                    places = placesDataRepository.getAllPlacesWithCityAndImages()
+                )
             }
+        }
+    }
 
+    fun addVisit(user: User) {
+        viewModelScope.launch {
+            visitsDataRepository.insertVisit(Visit(userId = user.id, placeId = uiState.value.currentSelectedPlace.place.id, dateVisited = LocalDateTime.now()))
+        }
+    }
+
+    fun getVisitsByUserIdAndPlaceId(user: User) {
+        viewModelScope.launch {
+            visitsDataRepository.getAllVisitDatesByUserIdAndPlaceId(user.id, uiState.value.currentSelectedPlace.place.id)
         }
     }
 
